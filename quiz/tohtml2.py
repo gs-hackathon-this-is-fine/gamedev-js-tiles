3fecbcba8aeb7baf87ca459ca7bcdb7768cb3067
Perlik <mp429581@students.mimuw.edu.pl>
2022-11-26 06:25:17 +0100

    quiz ileś

--- quiz/tohtml2.py
+++ quiz/tohtml2.py
@@ -50,7 +50,11 @@ for i in range(len(questions)):
 
     html += """
     <button type="submit" id="send">Zatwierdź</button>
-</form>"""
+</form>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async
+    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+</script>"""
     htmls += [html]
 
 file = open(f"quiz.js", "w")
