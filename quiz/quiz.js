d622d22ffa1b6a69670f2b3d9fabc138f7f34ae7
Mikołaj Komar <mikokomar@op.pl>
2022-11-26 03:12:26 +0100

    Some other things

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let quiz = `
+<p>Czy istnieje zbiór nieprzeliczalny mocy mniejszej niż continuum?</p>
+
+<form>
+    <input type="radio" id="1" value="1" name="ans">
+    <label for="1">Tak</label><br>
+
+    <input type="radio" id="2" value="2" name="ans">
+    <label for="2">Nie</label><br>
+
+    <input type="radio" id="3" value="3" name="ans">
+    <label for="3">To zależy</label><br>
+
+    <input type="radio" id="4" value="4" name="ans">
+    <label for="4">\(\forall_{x \in \mathbb R} x^2 \geq 0\)</label><br>
+
+    <button type="submit">Zatwierdź</button>
+</form>`
